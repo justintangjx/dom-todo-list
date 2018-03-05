89f2e9ff4b0ca13110acdee140540698b6ad8ede
“justintangjx” <just.think.tang@gmail.com>
2018-03-05 17:21:52 +0800

    first draft

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-tasks = ["buy milk", "eat dinner", "nail javascript"]
+var tasks = ["buy milk", "eat dinner", "nail javascript"];
+
+var todoList = document.querySelectorAll("ul")[0];
+var todos = document.querySelectorAll("li");
+
+var newTodo = document.createElement("li");
+newTodo.textContent = "get fish";
+
+todoList.appendChild(newTodo);
+
+var form = document.getElementsByTagName("form")[0];
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  var newItemtoadd = document.createElement("li");
+  newItemtoadd.textContent = document.getElementById("newtodo").value;
+  todoList.appendChild(newItemtoadd);
+});
+
+for (let i = 0; i < todos.length; i++) {
+  todos[i].addEventListener("click", function() {
+    if (todos[i].classList.contains("done")) {
+      todos[i].classList.remove("done");
+    } else {
+      todos[i].classList.add("done");
+    }
+  });
+}
