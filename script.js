e043be8f9d36898595b79133555efa3a00a4c20e
“justintangjx” <just.think.tang@gmail.com>
2018-03-08 09:17:17 +0800

    updated with crossing out of inputs

--- script.js
+++ script.js
@@ -4,29 +4,44 @@ var todoList = document.querySelectorAll("ul")[0];
 var todos = document.querySelectorAll("li");
 
 var newTodo = document.createElement("li");
-newTodo.textContent = "get fish";
+// newTodo.textContent = "get fish";
 
-todoList.appendChild(newTodo);
-
+// todoList.appendChild(newTodo);
+function clearContent() {
+  document.getElementById("newtodo").value = " ";
+}
 var form = document.getElementsByTagName("form")[0];
-
 form.addEventListener("submit", function(event) {
   event.preventDefault();
   var newItemtoadd = document.createElement("li");
   newItemtoadd.textContent = document.getElementById("newtodo").value;
-  todoList.appendChild(newItemtoadd);
+  newItemtoadd.addEventListener("click", markAsDone);
+  todoList.appendChild(newItemtoadd).clearContent;
 });
 
-
-
 for (let i = 0; i < todos.length; i++) {
-  todos[i].addEventListener("click", function() {
-      
-    if (todos[i].classList.contains("done")) {
-      todos[i].classList.remove("done");
-    } else {
-      todos[i].classList.add("done");
-    }
-  });
-};
+  todos[i].addEventListener("click", markAsDone);
+}
 
+function markAsDone(e) {
+  if (e.target.classList.contains("done")) {
+    e.target.classList.remove("done");
+  } else {
+    e.target.classList.add("done");
+  }
+}
+
+// function markAsDone(event) {
+//   event.target.classList.add("done");
+// }
+
+// 2. getting user input and displaying new todo
+// var form = document.getElementsByTagName("form")[0];
+// form.addEventListener("submit", function(event) {
+//   event.preventDefault();
+
+//   var userInput = form.elements["new todo"].value;
+//   createAndDisplayItem(userInput);
+
+//   form.elements["new todo"].value = "";
+// });
